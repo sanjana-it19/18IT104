2128c471797913a033d6da8ab6e716ea9ab7f3b7
sanjana-it19 <sanjana.it19@bitsathy.ac.in>
2019-09-06 12:24:11 +0530

    first commit

---
+++ num.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+printf("      1\n");
+printf("    1 2 1\n");
+printf("  1 2 3 1 2 3\n");
+printf("1 2 3 1 2 3 1\n");
+}
---
+++ op.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+printf("%d",20-5);
+printf("%d",30*2);
+printf("%d",25/2);
+}
